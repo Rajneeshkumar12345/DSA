3844c2a2980418b74bfb800513919c3b5732ca83
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-02-24 20:07:20 +0530

    add

---
+++ dp/1_FUNCTION_AND_ARRAY/binarySearch/Main.java
@@ -0,0 +1,22 @@
+
+// public static int binarySearch( int [] arr, int x){
+//     int li = 0;
+//     int ri = arr.length - 1;
+
+//     while( li <= ri){
+//         int mid = (li + ri) / 2;
+//         if( arr[mid] == x){
+//             return mid;
+//         }  else if( arr[mid] > x){
+//             ri = mid - 1;
+//         } else if( arr[mid] < x){
+//             li = mid + 1;
+//         }
+//     }
+//     return -1;
+// }
+
+// public static void main(String[] args){
+
+    
+// }
