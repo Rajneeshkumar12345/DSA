da64e2cef11a6d8d00478cfa91c47d20aa5c196a
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-02-25 18:04:20 +0530

    added

--- dp/1_FUNCTION_AND_ARRAY/binarySearch/Main.java
+++ dp/1_FUNCTION_AND_ARRAY/binarySearch/Main.java
@@ -1,22 +1,34 @@
+  import java.io.*;
+  import java.util.*;
+   public class Main{
+public static int binarySearch( int [] arr, int x){
+    int li = 0;
+    int ri = arr.length - 1;
 
-// public static int binarySearch( int [] arr, int x){
-//     int li = 0;
-//     int ri = arr.length - 1;
+    while( li <= ri){
+        int mid = (li + ri) / 2;
+        if( arr[mid] == x){
+            return mid;
+        }  else if( arr[mid] > x){
+            ri = mid - 1;
+        } else if( arr[mid] < x){
+            li = mid + 1;
+        }
+    }
+    return -1;
+  }
 
-//     while( li <= ri){
-//         int mid = (li + ri) / 2;
-//         if( arr[mid] == x){
-//             return mid;
-//         }  else if( arr[mid] > x){
-//             ri = mid - 1;
-//         } else if( arr[mid] < x){
-//             li = mid + 1;
-//         }
-//     }
-//     return -1;
-// }
+      public static void main(String[] args){
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
 
-// public static void main(String[] args){
-
+      int [] arr = new int[n];
+      for( int i = 0;i<n;i++){
+          arr[i] = scn.nextInt();
+      }
+      int x = scn.nextInt();
+      int ans = binarySearch(arr, x);
+      System.out.println(ans);
     
-// }
+  }
+}
