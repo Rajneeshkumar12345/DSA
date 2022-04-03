38230161776d28837fe0360ffbc9a0d2c5b0718b
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-04-04 00:30:22 +0530

    add

--- dp/10_BINARY_SEARCH_TREE/LCA_LC235/Main.java
+++ dp/10_BINARY_SEARCH_TREE/LCA_LC235/Main.java
@@ -13,13 +13,13 @@ class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode curr = root;
         while(curr != null){
-            if(p.val < curr.val && q.val < curr.val){
+            if(p.val < curr.val && q.val < curr.val){ // dono se small hai search in left
                 curr = curr.left;
-            } else if(p.val > curr.val && q.val > curr.val){
+            } else if(p.val > curr.val && q.val > curr.val){  // dono se long hai search in right
                 curr = curr.right;
             }
             else {
-                return curr;
+                return curr;  // Dono ke beech me hoga 
             }
         }
         return null; 
