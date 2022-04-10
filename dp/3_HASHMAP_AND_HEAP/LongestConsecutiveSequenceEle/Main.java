994d13b50eb81dbf30eac5d704172a371ab6aaf0
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-04-10 21:24:32 +0530

    added

--- dp/3_HASHMAP_AND_HEAP/LongestConsecutiveSequenceEle/Main.java
+++ dp/3_HASHMAP_AND_HEAP/LongestConsecutiveSequenceEle/Main.java
@@ -47,7 +47,7 @@ public static void main(String[] args) throws Exception {
         }
     }
 
-    for(int i = maxStPt; i < maxStPt + maxLen; i++){  // proint like { 8, 9, 10, 11, 12} etc
+    for(int i = maxStPt; i < maxStPt + maxLen; i++){  // print like { 8, 9, 10, 11, 12} etc
         System.out.println(i);  
     }
 
